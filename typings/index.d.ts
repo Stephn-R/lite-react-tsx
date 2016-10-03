f4972e28f89cafd85e76bc1ba88b1b9ed535563b
Stephen Rodriguez <steprodriguez10@gmail.com>
2016-10-03 12:35:12 -0400

    remove typings

--- typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="globals/chai/index.d.ts" />
-/// <reference path="globals/core-js/index.d.ts" />
-/// <reference path="globals/enzyme/index.d.ts" />
-/// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/react-dom/index.d.ts" />
-/// <reference path="globals/react/index.d.ts" />
